3b94ad311240b7b201065347423bee0e04bd2d50
frenklinmici12 <frenklinmici12@gmail.com>
2026-05-25 22:29:57 -0500

    vite setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,61 @@
+/*
+  Author: Frenklin Mici
+  This file's purpose is to be the router. All it does
+  is set up the structure
+*/
+
+//import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+
+import './variables.css'
+
+//import pages here as i do them
+import Home from './pages/Home/Home.jsx';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>//
+      </Routes>
+    </Router>
+  );
+}
+
+export default App
+
+
+ /*
+
+ Example for using axios for fetch request.
+
+  const [msg, setMsg] = useState('Loading...')
+
+  useEffect(() => {
+    axios.get('http://localhost:5001/')
+      .then(res => setMsg(res.data.message))
+      .catch(err => setMsg('Error connecting to backend: ', err.data.message))
+  }, [])
+
+  return <h1>{msg}</h1>
+
+
+
+
+const fetchGame = async () => {
+  try {
+    const res = await fetch(url + "/api/" + gameId);
+    const data = await res.json();
+    setGame(data);
+  } catch (err) {
+    console.log("Error fetching game: ", err);
+  }
+};
+f
+useEffect() { fetchGame()
+}
+
+
+  */
