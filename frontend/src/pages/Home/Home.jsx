c17534184db16b88a9b77a40f040a16461e0106d
frenklinmici12 <frenklinmici12@gmail.com>
2026-06-08 18:54:38 -0500

    barebones UI

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,40 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 export default function Home() {
+    const aboutRef = useRef(null);
+    const homeRef = useRef(null);
+
+    const scrollToAbout = () => {
+        aboutRef.current.scrollIntoView({ behavior: "smooth" });
+    };
+    const scrollToHome = () => {
+        homeRef.current.scrollIntoView({ behavior: "smooth" });
+    };
+
+
     return (
         <>
-            <div>home page</div>
+            <nav>
+                <img src="../../public/cadenza_logo.png" alt="logo"></img>
+                <h3 onClick={scrollToHome}>Home</h3>
+                <h3 onClick={scrollToAbout}>About</h3>
+            </nav>
+
+            <div className="Home" ref={homeRef}>
+                <div className="home-left">
+                    <h1>
+                        Talk better.
+                        Feel better.
+                        Own the conversation with Cadenza.
+                    </h1>
+                    <button>Get Started Now!</button>
+                </div>
+                <div className="home-right">
+                    <img src="" alt="view"></img>
+                </div>
+            </div>
         </>
     )
 }
