138f3bbd2aa8ebad0733edee1995c9a908131707
frenklinmici12 <frenklinmici12@gmail.com>
2026-06-12 21:32:52 -0500

    chat UI

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import "../styling/Chat.css"
 
-export default function Home() {
+export default function Chat() {
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState(["hi there!", "hi"]); // placeholder chat value 
+
+    const navigate = useNavigate();
 
     return (
+        <>
+         <nav>
+            <img src="/cadenza_logo.png" alt="logo"></img>
+            
+            <div className="nav-links">
+                <h3 onClick={() => navigate("/")}>Home</h3>
+                <h3>My Profile</h3>
+                <h3>Exit Scenario</h3>
+            </div>
+        </nav>
+       
         <div className="container">
+       
           <div className="scenario-info">
             <h2>Scenario Info:</h2>
             <ul>
@@ -18,7 +35,13 @@ export default function Home() {
           <div className="right-side">
             <h1>Discussion Title</h1>
             <div className="chatbox">
-                <p>this is the chatbox </p>
+                <div className="messages">
+                    {messages.map((msg, i) => (
+                        <div key={i} className="msg">
+                            {msg}
+                        </div>
+                    ))}
+                </div>
                 <div className="response">
                     <input type="text" placeholder="Form your response here..."></input>
                     <button>Send</button>
@@ -26,5 +49,6 @@ export default function Home() {
             </div>
           </div>
         </div>
+        </>
     )
 }
