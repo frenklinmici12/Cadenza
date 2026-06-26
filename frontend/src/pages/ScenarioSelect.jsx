d0cff76eaf6fdca2b844f25ec99412ecc60b4b48
frenklinmici12 <frenklinmici12@gmail.com>
2026-06-26 16:20:07 -0500

    built scenario select UI

--- frontend/src/pages/ScenarioSelect.jsx
+++ frontend/src/pages/ScenarioSelect.jsx
@@ -4,8 +4,33 @@ import { useNavigate } from "react-router-dom";
 
 import "../styling/ScenarioSelect.css"
 
+function Scenario({ scenario }) {
+    const navigate = useNavigate();
+    return (
+        <div className="scenario" onClick={() => navigate("/chat/" /*+ scenario.name)*/)}>
+            <h5>{scenario.situation}</h5>
+            <p>Goal: {scenario.goal}</p>
+        </div>
+    )
+}
+
 export default function ScenarioSelect() {
     const backend = import.meta.env.VITE_API_URL;
+
+    const [scenarios, setScenarios] = useState([
+        {
+            name: "Job-Interview",
+            situation: "Talking to a recruitor for SWE Internship",
+            goal: "Make a good impression"
+        },
+        {
+            name: "First Date",
+            situation: "Dinner date with girl.",
+            goal: "Make a good impression"
+        }
+    ]);
+
+
 
     const navigate = useNavigate();
 
@@ -20,8 +45,16 @@ export default function ScenarioSelect() {
             </div>
         </nav>   
         
-        <div className="container">
-            this is the scenario slect
+        <div className="scenario-container">
+            <h1>Select a Scenario</h1>
+
+             {scenarios.map((scenario) => (
+                <Scenario
+                    key={scenario.name}
+                    scenario={scenario}
+                />
+            ))}
+
         </div>
         </>
     )
