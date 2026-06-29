5ec7b7c06d9744fb2f016b6b91f799974e313bfc
frenklinmici12 <frenklinmici12@gmail.com>
2026-06-29 00:33:44 -0500

    store scenario data in endpoints

---
+++ backend/data/scenarios.js
@@ -0,0 +1,25 @@
+const scenarios = [
+    {
+        name: "job_interview",
+        situation: "Interview For SWE Internship",
+        character: {
+            name: "Jane",
+            role: "SWE Recruiter",
+            personality: "Professsional, friendly"
+
+        },
+        goal: "Make a good impression in hopes of getting the position"
+    },
+    {
+        name: "first_date",
+        situation: "Dinner Date With Potential Romantic Interest.",
+        character: {
+            name: "Stacy",
+            role: "Potential romantic interest",
+            personality: "Kind, but also shy"
+        },
+        goal: "Make her feel comfortable with you"
+    }
+];
+
+export default scenarios;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import scenarios from "./data/scenarios.js"
 
 dotenv.config()
 
@@ -9,9 +10,30 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// just a test
 app.get("/", (req, res) => {
   res.json({ message: "Backend is running!" })
 })
+
+// store scenario data here to be fetched by chat client
+app.get("/api/scenarios", (req, res) => {
+  res.json(scenarios);
+});
+
+// find scenario by its id ex: first_date or job_interview
+app.get("/api/scenarios/:id", (req, res) => {
+    const scenario = scenarios.find(
+        s => s.name === req.params.id
+    );
+
+    if (!scenario) {
+        return res.status(404).json({
+            error: "Scenario not found"
+        });
+    }
+
+    res.json(scenario);
+});
 
 const PORT = process.env.PORT || 5000
 
